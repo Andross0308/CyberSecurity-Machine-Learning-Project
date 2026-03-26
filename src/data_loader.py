e1399f5ccb492ef06376c29dc312363ce6a7ab90
André Martins <acosmartins06@gmail.com>
2026-03-26 20:05:29 +0000

    Return the df

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,4 +5,5 @@ def get_open_file(name):
     df = pd.read_csv(config.DATA_FILE_PATH + name, sep=config.COMMA)
     df.drop(columns=config.DROP_COLUMNS, inplace=True)
     df[config.CLASS_COLUMN] = df[config.CLASS_COLUMN].map(config.CLASS_MAP)
+    return df
 
